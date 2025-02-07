a5023462d3a46ec46737a42d8b4fc0eadff7ba22
saynis <aishaabdifetah@gmail.com>
2025-02-07 16:02:16 +0300

    done

--- frontend/src/components/Users/Login.jsx
+++ frontend/src/components/Users/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
+import { Link } from 'react-router-dom'
 
 const Login = () => {
 
