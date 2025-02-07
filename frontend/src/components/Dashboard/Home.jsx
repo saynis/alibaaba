a37d435d689c46075050966b8b928476cfe6527e
saynis <aishaabdifetah@gmail.com>
2025-02-07 10:30:34 +0300

    done

--- frontend/src/components/Dashboard/Home.jsx
+++ frontend/src/components/Dashboard/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './Header'
-import Products from './products'
+import Products from './Products'
 import Footer from './Footer'
 
 
--- frontend/src/components/Dashboard/Products.jsx
+++ frontend/src/components/Dashboard/Products.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import OneProduct from './OneProduct'
+import OneProduct from './Oneproduct'
 
 
 
